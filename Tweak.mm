7a80b87116a1a4709dbd903d8bd7510321b6a40b
Cryptiiiic <liamwqs@gmail.com>
2020-02-27 00:53:02 -0800

    Use new lib completionHandler and update libjelbreklib

--- Tweak.mm
+++ Tweak.mm
@@ -9,11 +9,13 @@ JelBrekICU *jbicu = nil;
 -(void)_saveImageToPhotoLibrary:(UIImage *)image environmentDescription:(id)env
 {
     %orig;
-    NSString *url = [[JelBrekICU new] uploadUIImage:image jelbrekKey:@"b326ae99-b778-4b84-91f5-595402e05a35" siteURL:[NSURL URLWithString:@"http://jelbrek.icu/upload"]];
-    if([jbicu logging])
-        NSLog(@"JelbrekICU: url: %@", url);
-    if(url)
-        pasteboard.string = url;
+    [[JelBrekICU new] uploadUIImage:image jelbrekKey:@"b326ae99-b778-4b84-91f5-595402e05a35" siteURL:[NSURL URLWithString:@"http://jelbrek.icu/upload"] completionHandler:^(NSString *url)
+    {
+        if([jbicu logging])
+            NSLog(@"JelbrekICU: url: %@", url);
+        if(url)
+            pasteboard.string = url;
+    }];
 }
 
 %end
@@ -21,7 +23,7 @@ JelBrekICU *jbicu = nil;
 %ctor
 {
     jbicu = [JelBrekICU new];
-    [jbicu setLogging:YES];
+    //[jbicu setLogging:YES];
     if([jbicu logging])
         NSLog(@"JelbrekICU: Init");
 }
